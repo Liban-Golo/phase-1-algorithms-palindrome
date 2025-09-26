63151d17bf8e1c32529b290d3d75850bb3ad65e2
Liban Golo <liban.golo@student.moringaschool.com>
2025-09-26 18:12:56 +0300

    palindrine1

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
+
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let i = 0; i < word.length / 2; i++) {
+    if (word[i] !== word[word.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  1. Loop from start of word to the middle
+  2. Compare the letter at position i with the letter at the mirrored position from the end
+  3. If any pair doesn't match, return false
+  4. If all pairs match, return true
 */
 
 /*
   Add written explanation of your solution here
+  The function checks if a word reads the same forwards and backwards.
+  It compares characters from the start and end moving inward. 
+  If all pairs match, the word is a palindrome.
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +34,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("madam"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("palindrome"));
 }
 
 module.exports = isPalindrome;
